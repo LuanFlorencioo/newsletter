dddc183daac222e09ba911d1910140f021572f8d
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-17 19:42:24 -0300

    feat: create List component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Illustration from "./Illustration";
 import Title from "./Title";
 import Text from "./Text";
+import List from "./List";
 
 export {
   Illustration,
   Title,
   Text,
+  List,
 }
