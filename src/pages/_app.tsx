1cbdb4ad3a9f872b6ea5d2a27f5eac3d12d8012d
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-17 12:53:00 -0300

    feat: add default styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { font } from "@/styles";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <main className={font}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default App;
