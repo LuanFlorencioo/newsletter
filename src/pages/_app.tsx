6b2f73c294fad352acd9cfa2688fa465f98ee2ca
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-18 00:03:40 -0300

    feat: add context in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { font } from "@/styles";
+import { AppProvider } from "@/contexts";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <main className={font}>
-      <Component {...pageProps} />
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
     </main>
   )
 }
